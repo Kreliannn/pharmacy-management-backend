aea5993dfeb5181af5ed85640f754b72ac0233b3
Kreliannn <krelianquimson@gmail.com>
2025-06-04 15:41:35 +0800

    add automatic deduct stocks

--- controller/transaction.controller.ts
+++ controller/transaction.controller.ts
@@ -2,10 +2,12 @@ import { request } from "http";
 import { transactionInterface, getTransactionInterface } from "../types/transaction.type";
 import { Request, Response } from "express";
 import { getTransactionService, createTransactionService } from "../services/transaction.service";
+import { updateProductQuantityService } from "../services/product.service";
 
 export const createTransactionController = async (request: Request, response: Response) => {
     const transaction: transactionInterface = request.body.transaction;
     await createTransactionService(transaction);
+    await updateProductQuantityService(transaction.productName, transaction.quantity, "-")
     const newTransaction = await getTransactionService();
     response.send(newTransaction)
 }
